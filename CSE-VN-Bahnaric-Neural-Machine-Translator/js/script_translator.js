8b23f29669d6e0fbe24fdecf3ecb3caa547a1af6
ngvihoa <hoa.nguyen29072002@hcmut.edu.vn>
2022-06-30 14:59:21 +0700

    new edition 2

--- CSE-VN-Bahnaric-Neural-Machine-Translator/js/script_translator.js
+++ CSE-VN-Bahnaric-Neural-Machine-Translator/js/script_translator.js
@@ -42,5 +42,41 @@ document.querySelector(".uploaded-file button").addEventListener('click', functi
     document.querySelector(".uploaded-file").style.display = 'none';
 })
 
+//api translator
+$('#btn-translate').click(() => {
+    const data = {
+        'text': $('#input-txt').val(),
+        'model': 'M2M'
+    };
+
+    let btnSpeak = document.getElementById('btn-speak');
+
+    if(btnSpeak.classList.contains('no-display') == false){
+        btnSpeak.classList.add('no-display');
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: 'https://www.ura.hcmut.edu.vn/NMT/api2/translate/text',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        success: (result) => {
+            $('#output-txt').text(result.ResultObj.tgt.join('\n'));
+
+            $.ajax({
+                type: 'POST',
+                url: 'https://www.ura.hcmut.edu.vn/tts/speak',
+                contentType: 'application/json',
+                data: JSON.stringify({ text: result.ResultObj.tgt.join(' ') }),
+                success: (result) => {
+                    const audio = new Audio('data:audio/wav;base64,' + result.speech);
+                    btnSpeak.classList.remove('no-display');
+                    $('#btn-speak').click(() => audio.play());
+                }
+            });
+        }
+    });
+});
+
 
 
